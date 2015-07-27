d49d186da17b6157eae6574322c27e4fd7b728a9
Brian Elvesæter <brian.elvesater@sintef.no>
2015-07-27 16:08:08 +0200

    Updated source code to use Log4J for debugging.

--- file-montrac/src/main/java/net/modelbased/proasense/adapter/montrac/MontracFileAdapter.java
+++ file-montrac/src/main/java/net/modelbased/proasense/adapter/montrac/MontracFileAdapter.java
@@ -1,29 +1,49 @@
+/**
+ * Copyright (C) 2014-2015 SINTEF
+ *
+ *     Brian Elvesæter <brian.elvesater@sintef.no>
+ *     Shahzad Karamat <shazad.karamat@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package net.modelbased.proasense.adapter.montrac;
+
+import net.modelbased.proasense.adapter.file.AbstractFileAdapter;
 
 import eu.proasense.internal.ComplexValue;
 import eu.proasense.internal.SimpleEvent;
 import eu.proasense.internal.VariableType;
-import net.modelbased.proasense.adapter.file.AbstractFileAdapter;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.*;
 
-/**
- * Created by shahzad on 18.07.15.
- */
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+import org.apache.log4j.Logger;
+
+
 public class MontracFileAdapter extends AbstractFileAdapter {
+    final static Logger logger = Logger.getLogger(MontracFileAdapter.class);
 
-    public static void main(String[] args) throws IOException, InterruptedException {
 
-        new MontracFileAdapter();
-    }
-
-    MontracFileAdapter() throws IOException, InterruptedException {
-
+    public MontracFileAdapter() throws IOException, InterruptedException {
         scanDirectory(rootDirectoryPath, delayValue);
     }
 
@@ -33,8 +53,6 @@ public class MontracFileAdapter extends AbstractFileAdapter {
         File file = new File(filePath);
         Scanner scanner = new Scanner(file);
         Map<String, ComplexValue> properties = new HashMap<String, ComplexValue>();
-
-        ComplexValue complexValue = new ComplexValue();
 
         String values[] = scanner.nextLine().split(",");
 
@@ -57,11 +75,14 @@ public class MontracFileAdapter extends AbstractFileAdapter {
 
         System.out.println("timestamp er  " + timestamp);
         System.out.println("sensorId er  " + sensorId);
+
+        ComplexValue complexValue = new ComplexValue();
         complexValue.setValue(values[2]);
         complexValue.setType(VariableType.STRING);
         properties.put("event", complexValue);
         System.out.println("event = " + values[2]);
 
+        complexValue = new ComplexValue();
         complexValue.setValue(values[3]);
         complexValue.setType(VariableType.LONG);
         properties.put("shuttle", complexValue);
@@ -69,6 +90,7 @@ public class MontracFileAdapter extends AbstractFileAdapter {
 
         String leftPiece[] = values[4].split("=");
 
+        complexValue = new ComplexValue();
         complexValue.setValue(leftPiece[1]);
         complexValue.setType(VariableType.STRING);
         properties.put("leftPiece", complexValue);
@@ -76,13 +98,20 @@ public class MontracFileAdapter extends AbstractFileAdapter {
 
         String rightPiece[] = values[5].split("=");
 
+        complexValue = new ComplexValue();
         complexValue.setValue(rightPiece[1]);
         complexValue.setType(VariableType.STRING);
         properties.put("rightPiece", complexValue);
         System.out.println("rightPiece er = " + rightPiece[1]);
+        logger.debug("rightPiece er = " + rightPiece[1]);
 
         SimpleEvent event = this.outputPort.createSimpleEvent(sensorId, timestamp, properties);
+        logger.debug("SimpleEvent = " + event.toString());
         this.outputPort.publishSimpleEvent(event);
+    }
 
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        new MontracFileAdapter();
     }
 }
