3f7fdce26e1ac5735672429b224b7c93afd07547
Brian Elvesæter <brian.elvesater@sintef.no>
2015-09-30 17:28:13 +0200

    Added exec:java build configuration in POM files.

--- file-productionplan/src/main/java/net/modelbased/proasense/adapter/productionplan/ProductionPlanFileAdapter.java
+++ file-productionplan/src/main/java/net/modelbased/proasense/adapter/productionplan/ProductionPlanFileAdapter.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package net.modelbased.proasense.adapter;
+package net.modelbased.proasense.adapter.productionplan;
 
 import eu.proasense.internal.ComplexValue;
 import eu.proasense.internal.SimpleEvent;
